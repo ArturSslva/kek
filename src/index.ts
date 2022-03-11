99ae259d24448b27c79c9aac97fdb458f68dbc61
ArturS <arturs.dasilva21@gmail.com>
2022-03-11 14:26:01 -0300

    deploy

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
-app.listen( process.env.PORT || 8080, () =>{
+app.listen( process.env.PORT, () =>{
     console.log('API RODANDO');
 });
 
