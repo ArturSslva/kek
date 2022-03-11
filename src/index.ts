dca3d3065c89cd30dcd20684dc2656536fc101a5
ArturS <arturs.dasilva21@gmail.com>
2022-03-11 15:51:18 -0300

    final deploy

--- src/index.ts
+++ src/index.ts
@@ -7,16 +7,11 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
-// app.use(express.static(__dirname + '/dist'));
-
 app.listen( process.env.PORT || 8080, () =>{
     console.log('API RODANDO');
 });
 
-// app.route('/').get((request: Request, response: Response) =>{
-//     console.log('Entrou');
-//     response.sendFile(__dirname + "/index.html");
-// });
+// Aqui é feito o o console.log com o contato realizado pelo curriculo, podendo ser visualizado no heroku cli no momento
 
 app.post('/contactEmail', (request: Request, response: Response) => {
     const nameEmail = request.body.name;
@@ -27,13 +22,9 @@ app.post('/contactEmail', (request: Request, response: Response) => {
     console.log(emailAdress);
     console.log(emailContent);
     
-    
-    
-    response.send('form recebido');
     console.log("Teste");
 });
 
 app.get('/', (request: Request, response: Response) =>{
-    
-    return response.send('Entrou');
+    return response.send('API RODANDO');
 });
