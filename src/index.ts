be971cb3b09c8c57163495733090c9464c592158
ArturS <arturs.dasilva21@gmail.com>
2022-03-11 13:33:32 -0300

    primeiro commit

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import express, {Request, response, Response} from 'express';
+import cors from 'cors';
+import axios from 'axios'
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+app.listen( process.env.PORT || 8080, () =>{
+    console.log('API RODANDO');
+});
+
+app.post('/contactEmail', (request: Request, response: Response) => {
+    const nameEmail = request.body.name;
+    const emailContent = request.body.emailContent;
+    const emailAdress = request.body.emailAdress;
+
+    console.log(nameEmail);
+    console.log(emailAdress);
+    console.log(emailContent);
+    
+    
+    
+    response.send('form recebido');
+    console.log("Teste");
+});
+
+app.get('/', (request: Request, response: Response) =>{
+    console.log('Entrou');
+    response.sendFile(__dirname + "/index.html");
+});
